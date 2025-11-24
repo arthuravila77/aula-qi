fcf76e6dc5ec57caeed6ac1c55dc8693b547ae73
arthuravila77 <arthuravila77@gmail.com>
2025-11-24 11:52:40 -0300

    Add files via upload

---
+++ widgets/my_radio.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyRadio extends StatefulWidget {
+  const MyRadio({super.key});
+
+  @override
+  State<MyRadio> createState() => _MyRadioState();
+}
+
+class _MyRadioState extends State<MyRadio> {
+  int _value = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return RadioListTile(
+      groupValue: 1,
+      onChanged: (v)=> _value = v!,
+      value: _value, 
+      title: Text("Gênero"),
+    );
+  }
+}
